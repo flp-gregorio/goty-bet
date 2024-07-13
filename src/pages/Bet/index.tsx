445ddd952a3205bd0e694d9d5c18c86bdd88d0bf
douglasew <douglas_ewerton10@hotmail.com>
2024-07-13 16:32:02 -0300

    adding new pages

---
+++ src/pages/Bet/index.tsx
@@ -0,0 +1,10 @@
+// import { Container } from './styles';
+const Bet = () => {
+  return (
+    <div>
+      <h1>Bet screen</h1>
+    </div>
+  );
+};
+
+export default Bet;
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,10 @@
+// import { Container } from './styles';
+const Login = () => {
+  return (
+    <div>
+      <h1>Login screen</h1>
+    </div>
+  );
+};
+
+export default Login;
