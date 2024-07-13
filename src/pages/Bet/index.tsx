a6adec47707ced2bae90398793305cc9539a0d09
douglasew <douglas_ewerton10@hotmail.com>
2024-07-13 17:36:55 -0300

    fix tailwind

--- src/pages/Bet/index.tsx
+++ src/pages/Bet/index.tsx
@@ -1,8 +1,7 @@
-// import { Container } from './styles';
 const Bet = () => {
   return (
     <div>
-      <h1 className="font-bold text-9xl">Bet screen</h1>
+      <h1 className="text-9xl font-sans">Bet screen</h1>
     </div>
   );
 };
