0e71bf6b8935ed6a9f243083c816969c6249b71e
douglasew <douglas_ewerton10@hotmail.com>
2024-07-13 21:49:52 -0300

    footer fixed

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -1,19 +1,19 @@
 const CardComponent = () => {
-    return (
-        <div className="w-full h-full">
-            <div className="relative hover:grayscale transition-all duration-500 w-full h-full">
-                <img
-                    src="https://cdn.thegameawards.com/1/2023/11/baldurs_gate_3.jpg"
-                    alt="game image"
-                    className="block w-full h-full object-cover"
-                />
-                <div className="absolute inset-0 flex flex-col justify-end items-start text-white p-4">
-                    <h2 className="text-2xl uppercase font-bold">Game Name</h2>
-                    <h3 className="text-lg uppercase font-thin">Game dev</h3>
-                </div>
-            </div>
+  return (
+    <div className="w-full h-full">
+      <div className="relative hover:grayscale transition-all duration-500 w-full h-full">
+        <img
+          src="https://cdn.thegameawards.com/1/2023/11/baldurs_gate_3.jpg"
+          alt="game image"
+          className="block w-full h-full object-cover"
+        />
+        <div className="absolute inset-0 flex flex-col justify-end items-start text-white p-4">
+          <h2 className="text-2xl uppercase font-bold">Game Name</h2>
+          <h3 className="text-lg uppercase font-thin">Game dev</h3>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default CardComponent;
--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -4,26 +4,12 @@ const FooterComponent = () => {
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
           <a className="text-gray-500">
-            <svg
-              fill="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              className="w-5 h-5"
-              viewBox="0 0 24 24"
-            >
+            <svg fill="currentColor" className="w-5 h-5" viewBox="0 0 24 24">
               <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
             </svg>
           </a>
           <a className="ml-3 text-gray-500">
-            <svg
-              fill="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              className="w-5 h-5"
-              viewBox="0 0 24 24"
-            >
+            <svg fill="currentColor" className="w-5 h-5" viewBox="0 0 24 24">
               <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
             </svg>
           </a>
@@ -31,9 +17,6 @@ const FooterComponent = () => {
             <svg
               fill="none"
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
               className="w-5 h-5"
               viewBox="0 0 24 24"
             >
@@ -45,9 +28,6 @@ const FooterComponent = () => {
             <svg
               fill="currentColor"
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="0"
               className="w-5 h-5"
               viewBox="0 0 24 24"
             >
