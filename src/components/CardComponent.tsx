4eb5d021c1d1352fd2c57d379fc35a9a4af1d5aa
flp-gregorio <felipealvesgregorio01@gmail.com>
2024-07-13 21:38:55 -0300

    Card component added

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -1,21 +1,19 @@
 const CardComponent = () => {
     return (
-      <div className="flex gap-10 font-inter mt-10 justify-center">
-        <div className="bg-white shadow-2xl " style={{ width: "300px" }}>
-          <div className="relative hover:grayscale transition-all duration-500">
-            <img
-              src="https://cdn.thegameawards.com/1/2023/11/baldurs_gate_3.jpg"
-              alt="game image"
-              className="block w-full rounded-lg"
-            />
-            <div className="absolute inset-0 flex flex-col justify-end items-start text-white p-4">
-              <h2 className="text-2xl uppercase font-bold">Game Name</h2>
-              <h3 className="text-lg uppercase font-thin">Game dev</h3>
+        <div className="w-full h-full">
+            <div className="relative hover:grayscale transition-all duration-500 w-full h-full">
+                <img
+                    src="https://cdn.thegameawards.com/1/2023/11/baldurs_gate_3.jpg"
+                    alt="game image"
+                    className="block w-full h-full object-cover"
+                />
+                <div className="absolute inset-0 flex flex-col justify-end items-start text-white p-4">
+                    <h2 className="text-2xl uppercase font-bold">Game Name</h2>
+                    <h3 className="text-lg uppercase font-thin">Game dev</h3>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     );
-  };
-  
-  export default CardComponent;
+};
+
+export default CardComponent;
--- src/pages/Bet/index.tsx
+++ src/pages/Bet/index.tsx
@@ -1,20 +1,23 @@
-import FooterComponent from "../../components/FooterComponent";
-import CardComponent from "../../components/CardComponent";
+import CardComponent from '../../components/CardComponent';
+import FooterComponent from '../../components/FooterComponent';
 
 const Bet = () => {
-  return (
-    <>
-      <div className="flex">
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-      </div>
-      <FooterComponent />
-    </>
-  );
+    return (
+        <>
+        <div className='h-full w-full bg-black'>
+          <div className="w-8/12 grid grid-cols-3 gap-3 m-auto">
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+          </div>
+        </div>
+          <FooterComponent />
+        </>
+    );
 };
 
 export default Bet;
+
