d4664eef0e4f8ff3bf0c85a21400fd5bc2d4249f
flp-gregorio <felipealvesgregorio01@gmail.com>
2024-07-13 23:54:00 -0300

    fixed homescreen font again

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import { RouterProvider } from "react-router-dom";
+import { useEffect } from "react";  
 import router from "./routes/Router";
 
+const useGoogleFonts = () => {
+  useEffect(() => {
+    const link = document.createElement('link');
+    link.href = 'https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap';
+    link.rel = 'stylesheet';
+    document.head.appendChild(link);
+  }, []);
+};
+
 function App() {
+  useGoogleFonts();
   return (
     <div>
       <RouterProvider router={router} />
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -36,7 +36,7 @@ const Home = () => {
         />
       </div>
       <div className=" sm:20 w-full lg:w-1/2 uppercase font-roboto-mono">
-        <div className="mt-6 text-blue-500 text-center">
+        <div className="mt-6 text-center">
           <div className="container px-5 mx-auto flex flex-wrap">
             <div className="w-full sm:p-4 px-4 mb-6 ">
               <h1 className="title-font font-bold text-2xl mb-2 text-gray-100 tracking-wider">
