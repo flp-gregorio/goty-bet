14ea5ad9f1493af615d56cf916b3a47dcfb48e65
flp-gregorio <felipealvesgregorio01@gmail.com>
2024-07-14 20:42:03 -0300

    added register page

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import Register from "../pages/Register"; 
 import Nominees from "../pages/Nominees";
 
 const router = createBrowserRouter([
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
   {
     path: "login",
     element: <Login />,
+  },
+  {
+    path: "register",
+    element: <Register />,
   },
 ]);
 
