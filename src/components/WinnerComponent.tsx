cfa0f95c7e4384052c4de80bb6c70fbcede45884
flp-gregorio <felipealvesgregorio01@gmail.com>
2024-07-15 02:39:26 -0300

    Fixed the winner component

--- src/components/WinnerComponent.tsx
+++ src/components/WinnerComponent.tsx
@@ -1,29 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
-import jsonData from "../assets/data.json";
-import { CategoriesData, NomineeData } from '../@types/NomineeType';
+import { NomineeData } from '../@types/NomineeType';
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
-const WinnerComponent: React.FC = () => {
-    const [selectedCategory, setSelectedCategory] = useState<string>(Object.keys(jsonData)[0]);
+type WinnerComponentProps = {
+    category: string;
+    data: NomineeData[];
+};
 
-    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setSelectedCategory(event.target.value);
-    };
-
-    const createChartData = (category: keyof CategoriesData) => {
-        const nominees: NomineeData[] = jsonData[category];
-        const labels = nominees.map(nominee => nominee.Nominee);
-        const data = nominees.map(nominee => nominee.Votes);
+const WinnerComponent: React.FC<WinnerComponentProps> = ({ category, data }) => {
+    const createChartData = () => {
+        const labels = data.map(nominee => nominee.Nominee);
+        const votes = data.map(nominee => nominee.Votes);
 
         return {
             labels: labels,
             datasets: [
                 {
                     label: 'Votes',
-                    data: data,
+                    data: votes,
                     backgroundColor: 'rgba(75, 192, 192, 0.2)',
                     borderColor: 'rgba(75, 192, 192, 1)',
                     borderWidth: 1,
@@ -40,7 +37,7 @@ const WinnerComponent: React.FC = () => {
             },
             title: {
                 display: true,
-                text: `${selectedCategory} Nominee Votes Bar Chart`,
+                text: `${category} Nominee Votes Bar Chart`,
             },
         },
     };
@@ -48,17 +45,8 @@ const WinnerComponent: React.FC = () => {
     return (
         <div>
             <h2>Winner Component</h2>
-            <select onChange={handleChange} value={selectedCategory}>
-                {Object.keys(jsonData).map(category => (
-                    <option key={category} value={category}>
-                        {category}
-                    </option>
-                ))}
-            </select>
-            <div>
-                <h3>{selectedCategory}</h3>
-                <Bar data={createChartData(selectedCategory as keyof CategoriesData)} options={options} />
-            </div>
+            <h3>{category}</h3>
+            <Bar data={createChartData()} options={options} />
         </div>
     );
 };
