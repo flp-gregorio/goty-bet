815f50547495c1066854ec49508fa49dd858ff23
flp-gregorio <felipealvesgregorio01@gmail.com>
2024-07-21 17:06:40 -0300

    build: connected to api

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/Router";
 
-function App() {
-  return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
 
+  callApi() {
+    fetch("http://localhost:3000/test")
+      .then((res) => res.text())
+      .then((res) => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callApi();
+  }
+
+  render() {
+    return (
+      <div>
+        <RouterProvider router={router} />
+        <p>{this.state.apiResponse}</p>
+      </div>
+    );
+  }
+}
 export default App;
