1bea918a32fcf3eb474c96eb147e31df7f54cadb
flp-gregorio <felipealvesgregorio01@gmail.com>
2024-07-24 15:30:02 -0300

    style: button and input changes

--- src/components/ButtonComponent.tsx
+++ src/components/ButtonComponent.tsx
@@ -10,7 +10,7 @@ const ButtonComponent = ({ text, ...rest }: Props) => {
       className="bg-orange-700 text-white min-w-32 px-4 py-1 mb-6 uppercase font-bold hover:bg-orange-800 hover:text-white transition-all duration-300"
       style={{
         clipPath:
-          "polygon(0px 0px, calc(100% - 18px) 0px, 100% 18px, 100% 100%, 18px 100%, 0px calc(100% - 18px))",
+          "polygon(0px 0px, 100% 0px, 100% 14px, 100% 100%, 14px 100%, 0px calc(100% - 14px))",
         padding: "9px 32px 9px 39px",
       }}
     >
