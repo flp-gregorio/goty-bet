bdc38f21ade97b04d47c21dd6d175641d4e36364
douglasew <douglas_ewerton10@hotmail.com>
2024-07-25 20:57:27 -0300

    change forms again

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
-import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/Router";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-  }
-
-  callApi() {
-    fetch("http://localhost:3000/test")
-      .then((res) => res.text())
-      .then((res) => this.setState({ apiResponse: res }));
-  }
-
-  componentDidMount() {
-    this.callApi();
-  }
-
-  render() {
-    console.log(this.state.apiResponse);
-    return (
-      <div>
-        <RouterProvider router={router} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
+
 export default App;
--- src/pages/Auth/Register/index.tsx
+++ src/pages/Auth/Register/index.tsx
@@ -1,9 +1,9 @@
 import { useForm } from "react-hook-form";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
+import { Link } from "react-router-dom";
 import ButtonComponent from "../../../components/ButtonComponent";
 import InputComponent from "../../../components/InputComponent";
 import LayoutAuthComponent from "../../../components/Layouts/LayoutAuthComponent";
+import api from "../../../lib/api";
 
 interface IFormInput {
   username: string;
@@ -13,40 +13,13 @@ interface IFormInput {
 }
 
 const Register = () => {
-  const nav = useNavigate();
-  const { register, handleSubmit, setError, watch } = useForm<IFormInput>();
-  const password = watch("password");
+  //const nav = useNavigate();
+  const { register, handleSubmit } = useForm<IFormInput>();
 
   const onSubmit = async (data: IFormInput) => {
-    try {
-      const response = await axios.post('http://localhost:3000/register', {
-        username: data.username,
-        email: data.email,
-        password: data.password,
-      });
-      // Assuming the response contains user data and a token
-      console.log(response.data);
-      // Save token to localStorage or state management
-      localStorage.setItem('token', response.data.token);
-      nav("/nominees");
-    } catch (error) {
-      if (axios.isAxiosError(error) && error.response) {
-        const { data } = error.response;
-        // Handle specific error messages from the server
-        if (data.message) {
-          alert(data.message);
-        }
-        // Set form errors for specific fields
-        if (data.errors) {
-          data.errors.forEach((err: { field: string; message: string }) => {
-            setError(err.field as keyof IFormInput, { message: err.message });
-          });
-        }
-      } else {
-        console.error('An unexpected error occurred:', error);
-        alert('An unexpected error occurred. Please try again later.');
-      }
-    }
+    await api.post("/users", data).then((res) => {
+      console.log(res);
+    });
   };
 
   return (
@@ -81,10 +54,7 @@ const Register = () => {
           label="Confirm Password"
           type="password"
           placeholder="Confirm Your Password"
-          {...register("confirmPassword", {
-            validate: value =>
-              value === password || "Passwords do not match",
-          })}
+          {...register("confirmPassword")}
         />
         <div className="mt-6 w-40 mx-auto">
           <ButtonComponent text="Register" type="submit" />
